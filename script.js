e11b68052954626f9affddd45053d92d3b5fdb2f
hulyatrn <66426264+hulyatrn@users.noreply.github.com>
2022-01-05 22:15:54 +0300

    siragecesi

---
+++ script.js
@@ -0,0 +1,11 @@
+    (function () {
+        var options = {
+            whatsapp: "905558292763",
+            call_to_action: "Merhaba, nasıl yardımcı olabilirim?",
+            position: "right",
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
